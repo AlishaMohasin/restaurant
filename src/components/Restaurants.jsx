27c414fe31d27b1b561e113d5e4ed5dfc5d56106
Alisha <97678545+AlishaMohasin@users.noreply.github.com>
2022-05-28 22:48:56 +0530

    Add files via upload

---
+++ src/components/Restaurants.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Restaurant from './Restaurant'
+
+const Restaurants = (props) => {
+  return (
+    <div className="container divider">
+       
+    <div className="cards">
+    {props.Restaurant.map((restaurants)=>( 
+   
+   
+<Restaurant key={restaurants.id} restaurants={restaurants}/>
+   
+   
+ ))}
+       
+     
+       
+       
+    </div>
+</div>
+    
+  )
+}
+
+export default Restaurants
